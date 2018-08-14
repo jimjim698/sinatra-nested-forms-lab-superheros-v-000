1fa9f6641ce48994f7ec51974608a58bec2f20e7
James Lotruglio <jimlotruglio@yahoo.com>
2018-08-14 20:59:19 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,13 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
-
-    get '/' do
+    get '/' do 
       erb :super_hero
-    end
+    end 
 
-    post'/team' do
+    post '/team' do 
       erb :team
     end 
+
 
 end
